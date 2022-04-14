bb3d2d1bdbc26ddc79a1533d3f25717ded909df5
Jessica Guico <jessicamcguico@gmail.com>
2022-04-13 21:40:10 -0500

    calendar rendered on homepage

---
+++ client/src/components/CalendarContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Box from "@mui/material/Box"
+import Calendar from 'react-calendar'
+
+const CalendarContainer = () => {
+    return (
+        <Box>
+            <Calendar/>
+        </Box>
+    )
+}
+
+export default CalendarContainer
---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import 'react-calendar/dist/Calendar.css';
+import CalendarContainer from "../components/CalendarContainer";
+
+const Home = () => {
+    return (
+        <>
+            <h1>testing home</h1>
+            <CalendarContainer />
+        </>
+    )
+}
+
+export default Home
