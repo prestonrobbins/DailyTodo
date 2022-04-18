ef12e52fdd2703175751d88cf58b6937a693f1bb
Jessica Guico <jessicamcguico@gmail.com>
2022-04-17 22:44:49 -0500

    style home/calendar, added start day button

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { Box, Typography, Button } from '@mui/material';
 import 'react-calendar/dist/Calendar.css';
 import CalendarContainer from "../components/CalendarContainer";
 
 const Home = () => {
     return (
-        <>
-            <h1>testing home</h1>
+        <Box
+            sx={{ 
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "column",
+            }}
+        >
+            <Typography component={'h1'} variant='h3'>Welcome to your Daily ToDos!</Typography>
             <CalendarContainer />
-        </>
+            <Button variant="contained" disableElevation>
+                Start Your Day
+            </Button>
+        </Box>
     )
 }
 
